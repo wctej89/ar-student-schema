3dd63566f7a9c99fc0f1cc9710c76a269ba9ad09
Apprentice <apprentice@devbootcamp.com>
2013-05-09 17:44:43 -0700

    DONE SON

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 require_relative '../../db/config'
+require 'date'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /.+@.+\.\S{2,}/, :message => "Please enter a valid email address."}
+  validates :email, :uniqueness => true
+  validates :age, :numericality => {:greater_than => 5}
+  validate :phone_number_must_contain_more_than_nine_digits
+
+  def age
+    Time.now.year - birthday.year - ((Time.now.month > birthday.month || (Time.now.month == birthday.month && Time.now.day >= birthday.day)) ? 0 : 1 )
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def phone_number_must_contain_more_than_nine_digits
+    if phone.split(//).select{|d| d.to_i if d.to_i > 0 || d == "0" }.count < 9
+      errors.add(:phone, "Phone number must contain more than 9 digits.")
+    end
+  end
+end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,14 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+  create_table :students do |column|
+    column.integer  :id
+    column.string   :first_name
+    column.string   :last_name
+    column.string   :gender
+    column.date     :birthday
+    column.string   :email
+    column.string   :phone
+    end
   end
 end
